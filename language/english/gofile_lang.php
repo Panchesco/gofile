0d3a4dcba97c40f63992bd6d2a27e3c743ed9daa
Richard Whitmer <richard@panchesco.com>
2017-01-18 17:08:32 -0700

    Moves messages to language file.

---
+++ language/english/gofile_lang.php
@@ -0,0 +1,21 @@
+<?php
+	
+$lang = array(
+
+
+'directory_no_access' => 'You do not have access to this directory',
+
+'file_could_not_download' => 'There was a problem and the file cannot be downloaded.',
+
+'file_id_invalid' => 'The file id is not valid',
+
+'user_cannot_upload_files' => 'You are not allowed to upload files',
+
+
+//----------------------------------------
+
+// Additional Key => Value pairs go here
+
+// END
+''=>''
+);
